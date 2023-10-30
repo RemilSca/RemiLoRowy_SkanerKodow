5f128ca8f74ffe90c22ea6e079d69475637058d9
RemilSca <kamildudek2001@gmail.com>
2023-10-30 13:41:37 +0100

    a

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import cv2
-import kivy
-kivy.require('2.2.0') # replace with your current kivy version !
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-
 import lora
 
 #Funkcja ktora decyduje czy czytamy kod kreskowy czy qr
@@ -37,48 +31,4 @@ def read_qrcode(frame):
 
     return frame, val
 
-
-#kawałek kodu ktory formatuje wyglad aplikacji
-
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
-
-#klasa odpowiedzialna za działanie guzika capture
-
-class CameraClick(BoxLayout):
-    def capture(self):
-        camera = self.ids['camera']
-        #camera.export_to_png("temp.png")
-        frame = cv2.imread('qr.jpg')
-        frame, code = detect(frame)
-        cv2.imwrite("temp.jpg", frame)
-        print(code)
-        lora.send(code)
-
-#klasa aplikacji
-
-class MyApp(App):
-
-    def build(self):
-        return CameraClick()
-
-#Glowna petla programu
-
-if __name__ == '__main__':
-    MyApp().run()
+print(detect(cv2.imread('kod.jpg')))
