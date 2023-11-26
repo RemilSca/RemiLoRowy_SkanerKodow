f841951cd12294ef433aa16e3dcb7890f46d7878
RemilSca <kamildudek2001@gmail.com>
2023-11-26 17:11:27 +0100

    guzik + led

--- lora.py
+++ lora.py
@@ -15,7 +15,7 @@ def write_read(x, lora):
 
 #Funkcja wysylajaca dane przez lora, numer portu usb bedzie sie zmieniac wiec trzeba zaimplementowac jakis wykrywacz
 def send(d):
-    lora = serial.Serial(port='COM5', baudrate=9600, timeout=0.1)
+    lora = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
     x = write_read('AT+MODE=TEST\n', lora)
     print(x)
     x = write_read(f'AT+TEST=TXLRSTR,"{d}"\n', lora)
