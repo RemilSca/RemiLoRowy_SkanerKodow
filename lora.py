1280e7295cc0ec86b8c44994134a7672cff2f3c1
RemilSca <kamildudek2001@gmail.com>
2024-01-06 21:45:39 +0100

    aha

--- lora.py
+++ lora.py
@@ -8,6 +8,7 @@ appkey = '592D888ADE8D3EB039F1CFC0673639D5'
 def write_read(x, lora):
 
     lora.write(bytes(x, 'utf-8'))
+    time.sleep(0.5)
     data = lora.readline()
     data = data.decode()
 
@@ -16,10 +17,13 @@ def write_read(x, lora):
 def start():
     lora = serial.Serial(port=port, baudrate=9600, timeout=None)
     x = write_read(f'AT+KEY=APPKEY,"{appkey}"', lora)
+    time.sleep(1)
     print(x)
     x = write_read(f'AT+MODE=LWOTAA', lora)
+    time.sleep(1)
     print(x)
     x = write_read(f'AT+JOIN', lora)
+    time.sleep(15)
     print(x)
     send('11')
 #Funkcja wysylajaca dane przez lora, numer portu usb bedzie sie zmieniac wiec trzeba zaimplementowac jakis wykrywacz
